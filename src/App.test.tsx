8a1e445377752b5fc6eaa02c5921e5a15eec2737
Ayelén Trigo <aye.trigo@hotmail.com>
2023-06-10 22:08:31 -0300

    conflict with context reference

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+// import App from './App';
+import Prueba from './Prueba';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Prueba />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Prueba from './Prueba'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Prueba />
   </React.StrictMode>
 );
 
